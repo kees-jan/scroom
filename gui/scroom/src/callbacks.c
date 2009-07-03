d97f4334246302fac4371f1b2333d297ab5cc24e
Kees-Jan Dijkzeul <kees-jan.dijkzeul@iae.nl>
2009-07-03 21:30:19 +0200

    Generated the gui

---
+++ gui/scroom/src/callbacks.c
@@ -0,0 +1,90 @@
+#ifdef HAVE_CONFIG_H
+#  include <config.h>
+#endif
+
+#include <gtk/gtk.h>
+
+#include "callbacks.h"
+#include "interface.h"
+#include "support.h"
+
+
+void
+on_new_activate                        (GtkMenuItem     *menuitem,
+                                        gpointer         user_data)
+{
+
+}
+
+
+void
+on_open_activate                       (GtkMenuItem     *menuitem,
+                                        gpointer         user_data)
+{
+
+}
+
+
+void
+on_save_activate                       (GtkMenuItem     *menuitem,
+                                        gpointer         user_data)
+{
+
+}
+
+
+void
+on_save_as_activate                    (GtkMenuItem     *menuitem,
+                                        gpointer         user_data)
+{
+
+}
+
+
+void
+on_quit_activate                       (GtkMenuItem     *menuitem,
+                                        gpointer         user_data)
+{
+
+}
+
+
+void
+on_cut_activate                        (GtkMenuItem     *menuitem,
+                                        gpointer         user_data)
+{
+
+}
+
+
+void
+on_copy_activate                       (GtkMenuItem     *menuitem,
+                                        gpointer         user_data)
+{
+
+}
+
+
+void
+on_paste_activate                      (GtkMenuItem     *menuitem,
+                                        gpointer         user_data)
+{
+
+}
+
+
+void
+on_delete_activate                     (GtkMenuItem     *menuitem,
+                                        gpointer         user_data)
+{
+
+}
+
+
+void
+on_about_activate                      (GtkMenuItem     *menuitem,
+                                        gpointer         user_data)
+{
+
+}
+
---
+++ gui/scroom/src/callbacks.h
@@ -0,0 +1,42 @@
+#include <gtk/gtk.h>
+
+
+void
+on_new_activate                        (GtkMenuItem     *menuitem,
+                                        gpointer         user_data);
+
+void
+on_open_activate                       (GtkMenuItem     *menuitem,
+                                        gpointer         user_data);
+
+void
+on_save_activate                       (GtkMenuItem     *menuitem,
+                                        gpointer         user_data);
+
+void
+on_save_as_activate                    (GtkMenuItem     *menuitem,
+                                        gpointer         user_data);
+
+void
+on_quit_activate                       (GtkMenuItem     *menuitem,
+                                        gpointer         user_data);
+
+void
+on_cut_activate                        (GtkMenuItem     *menuitem,
+                                        gpointer         user_data);
+
+void
+on_copy_activate                       (GtkMenuItem     *menuitem,
+                                        gpointer         user_data);
+
+void
+on_paste_activate                      (GtkMenuItem     *menuitem,
+                                        gpointer         user_data);
+
+void
+on_delete_activate                     (GtkMenuItem     *menuitem,
+                                        gpointer         user_data);
+
+void
+on_about_activate                      (GtkMenuItem     *menuitem,
+                                        gpointer         user_data);
