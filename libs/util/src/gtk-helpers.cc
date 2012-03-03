e9f2f3ec5ac716d9dd403c5f4449ca14e3ff9c7c
Kees-Jan Dijkzeul <kees-jan.dijkzeul@iae.nl>
2012-03-03 10:36:05 +0100

    Add a wrapper for passing boost::function objects to gtk functions

---
+++ libs/util/src/gtk-helpers.cc
@@ -0,0 +1,68 @@
+/*
+ * Scroom - Generic viewer for 2D data
+ * Copyright (C) 2009-2012 Kees-Jan Dijkzeul
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License, version 2, as published by the Free Software Foundation.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+#include <scroom/gtk-helpers.hh>
+
+typedef boost::shared_ptr<boost::function<bool()> > GtkFuncPtr;
+
+namespace Scroom
+{
+  namespace GtkHelpers
+  {
+    namespace Detail
+    {
+      class Wrapper
+      {
+      public:
+        boost::function<bool()> f;
+
+      public:
+        static gpointer create(const boost::function<bool()>& f);
+        Wrapper(const boost::function<bool()>& f);
+      };
+
+      Wrapper::Wrapper(const boost::function<bool()>& f)
+      :f(f)
+      {}
+
+      gpointer Wrapper::create(const boost::function<bool()>& f)
+      {
+        return new Wrapper(f);
+      }
+
+      static int gtkWrapper(gpointer data)
+      {
+        Wrapper* w = reinterpret_cast<Wrapper*>(data);
+        bool result = w->f();
+        if(!result)
+          delete w;
+        return result;
+      }
+    }
+
+    Wrapper::Wrapper(const boost::function<bool()>& f)
+        : f(&Detail::gtkWrapper), data(Detail::Wrapper::create(f))
+    {
+    }
+
+    Wrapper wrap(boost::function<bool()> f)
+    {
+      return Wrapper(f);
+    }
+  }
+}
+
---
+++ libs/util/test/gtkhelper-tests.cc
@@ -0,0 +1,76 @@
+/*
+ * Scroom - Generic viewer for 2D data
+ * Copyright (C) 2009-2012 Kees-Jan Dijkzeul
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License, version 2, as published by the Free Software Foundation.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+#include <scroom/gtk-helpers.hh>
+
+#include <gtk/gtk.h>
+
+#include <boost/test/unit_test.hpp>
+
+#include <boost/shared_ptr.hpp>
+#include <boost/weak_ptr.hpp>
+#include <boost/bind.hpp>
+
+using namespace Scroom::GtkHelpers;
+
+namespace
+{
+  class B
+  {
+  public:
+    typedef boost::shared_ptr<B> Ptr;
+    typedef boost::weak_ptr<B> WeakPtr;
+
+    static Ptr create() { return Ptr(new B()); }
+  };
+}
+
+static bool b(bool& in, B::Ptr)
+{
+  return in;
+}
+
+BOOST_AUTO_TEST_SUITE(Gtk_Helpers_Tests)
+
+BOOST_AUTO_TEST_CASE(function_returning_bool)
+{
+  bool in=true;
+  GtkFunction f = NULL;
+  gpointer data = NULL;
+  B::WeakPtr wb;
+
+  {
+    B::Ptr sb = B::create();
+    wb = sb;
+    Wrapper w = wrap(boost::bind(b,boost::ref(in),sb));
+    f = w.f;
+    data = w.data;
+  }
+  BOOST_CHECK(wb.lock());
+
+  bool result = (*f)(data);
+  BOOST_CHECK_EQUAL(true, result);
+  BOOST_CHECK(wb.lock());
+  in=false;
+  result = (*f)(data);
+  BOOST_CHECK_EQUAL(false, result);
+  BOOST_CHECK(!wb.lock());
+
+}
+
+BOOST_AUTO_TEST_SUITE_END()
+
