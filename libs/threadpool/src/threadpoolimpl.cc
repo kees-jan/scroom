9476a595f9cb5feb26098913edcb90d439d9a59a
Kees-Jan Dijkzeul <kees-jan.dijkzeul@iae.nl>
2013-05-10 16:42:07 +0200

    Fix #6: "waiting for %d threads to terminate" logging does not have a trailing newline

--- libs/threadpool/src/threadpoolimpl.cc
+++ libs/threadpool/src/threadpoolimpl.cc
@@ -1,6 +1,6 @@
 /*
  * Scroom - Generic viewer for 2D data
- * Copyright (C) 2009-2012 Kees-Jan Dijkzeul
+ * Copyright (C) 2009-2013 Kees-Jan Dijkzeul
  * 
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
@@ -95,7 +95,7 @@ namespace
     while(triesRemaining>0 && count>0)
     {
       boost::mutex::scoped_lock lock(mut);
-      printf("\nWaiting for %d threads to terminate", count);
+      printf("\nWaiting for %d threads to terminate\n", count);
 
       std::list<ThreadPool::ThreadPtr>::iterator cur = threads.begin();
       while(cur != threads.end())
