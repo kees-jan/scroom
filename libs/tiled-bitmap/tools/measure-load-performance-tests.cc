0f19391b70405be101bedaec90d0028b1be2cf21
Kees-Jan Dijkzeul <kees-jan.dijkzeul@iae.nl>
2014-07-16 12:38:35 +0200

    Non-recursive make - part 2

--- libs/tiled-bitmap/tools/measure-load-performance-tests.cc
+++ libs/tiled-bitmap/tools/measure-load-performance-tests.cc
@@ -16,7 +16,7 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
  */
 
-#include <measure-load-performance-tests.hh>
+#include "measure-load-performance-tests.hh"
 
 #include <time.h>
 
