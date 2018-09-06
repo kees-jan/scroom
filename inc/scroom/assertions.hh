f7e4c07a81584370680f7d252f9c5df2b1dcf915
Kees-Jan Dijkzeul <kees-jan.dijkzeul@iae.nl>
2018-09-06 08:18:36 +0200

    Separate compiler specific defines from assertions.hh

--- inc/scroom/assertions.hh
+++ inc/scroom/assertions.hh
@@ -9,12 +9,6 @@
 
 #include <sys/cdefs.h>
 
-#ifndef __clang__
-#define GCC_VERSION (__GNUC__ * 10000           \
-                     + __GNUC_MINOR__ * 100     \
-                     + __GNUC_PATCHLEVEL__)
-#endif
-
 namespace Scroom
 {
   namespace Utils
---
+++ inc/scroom/compiler.hh
@@ -0,0 +1,15 @@
+/*
+ * Scroom - Generic viewer for 2D data
+ * Copyright (C) 2009-2018 Kees-Jan Dijkzeul
+ *
+ * SPDX-License-Identifier: LGPL-2.1
+ */
+
+#pragma once
+
+#ifndef __clang__
+#define GCC_VERSION (__GNUC__ * 10000           \
+                     + __GNUC_MINOR__ * 100     \
+                     + __GNUC_PATCHLEVEL__)
+#endif
+
